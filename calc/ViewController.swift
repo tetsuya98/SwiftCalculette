03b95e58da13e0e93c49f07e211f928294da4156
Josselin <josselin@dock-d-09.sciences.etud.u-picardie.fr>
2020-10-22 12:19:54 +0200

    label + Pi

--- calc/ViewController.swift
+++ calc/ViewController.swift
@@ -9,10 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var userIsInTheMiddleOfTyping: Bool = false
+    @IBOutlet weak var display: UILabel!
 
     @IBAction func touchDigit(_ sender: UIButton) {
         let digit = sender.currentTitle!
-        print("\(digit) was used")
+        //print("\(digit) was used")
+        if userIsInTheMiddleOfTyping {
+            let currentDisplay = display!.text
+            display?.text = currentDisplay! + digit
+        }else{
+            display!.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
     }
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        if let mathematicalSymbol = sender.currentTitle {
+            switch  mathematicalSymbol {
+            case "π":
+                display!.text = String(Double.pi)
+            case "√":
+                let operand = Double(display!.text!)
+                display!.text = String(sqrt(operand!))
+            default:
+                break
+            }
+        }
+        userIsInTheMiddleOfTyping = false
+    }
+    
 }
 
